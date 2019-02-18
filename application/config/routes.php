f0dc52b0a2d8d186786dc6a053067072de7832ad
inganault <p.po.oo.on.n@gmail.com>
2019-02-18 21:10:25 +0900

    Make /exam/login as temporary index

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
-$route['test/(:any)'] = 'test/view/$1/aa';
+$route['default_controller'] = 'exam/login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
