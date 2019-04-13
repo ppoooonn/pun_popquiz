05e824905bae5aaa0b6df7baacf32ab33d1e08f7
inganault <p.po.oo.on.n@gmail.com>
2019-04-14 00:08:13 +0900

    Build.py with files watch

--- build.py
+++ build.py
@@ -3,6 +3,7 @@ import glob
 import os.path as path
 import subprocess
 import os, sys
+import time
 
 pugdirs = [
 	'application/views/exam/*.pug',
@@ -11,17 +12,28 @@ pugdirs = [
 exclude = {'common.pug'}
 defext = '.html'
 newext = '.php'
-for pugdir in pugdirs:
-	for pathn in glob.iglob(pugdir):
-		filename = path.basename(pathn)
-		if filename in exclude:
-			continue
-		ret = subprocess.call(['pug',pathn],shell=True)
-		if ret == 0:
+while 1:
+	for pugdir in pugdirs:
+		for pathn in glob.iglob(pugdir):
+			filename = path.basename(pathn)
+			out_path = path.splitext(pathn)[0] + newext
+			if filename in exclude:
+				continue
 			try:
-				os.unlink(path.splitext(pathn)[0] + newext)
+				if path.getmtime(out_path) > path.getmtime(pathn):
+					continue
 			except FileNotFoundError:
 				pass
-			os.rename(path.splitext(pathn)[0] + defext, path.splitext(pathn)[0] + newext)
-		else:
-			sys.exit(1)
+			ret = subprocess.call(['pug',pathn],shell=True)
+			if ret == 0:
+				try:
+					os.unlink(out_path)
+				except FileNotFoundError:
+					pass
+				os.rename(path.splitext(pathn)[0] + defext, out_path)
+			else:
+				sys.exit(1)
+	if len(sys.argv) > 1 and sys.argv[1] == 'watch':
+		time.sleep(0.5)
+		continue
+	break
